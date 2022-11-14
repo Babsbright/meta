311ebcc9c591530f5cb738a191d656bb030d9d2a
Tolulope Babatunde <babsbright69@gmail.com>
2022-11-14 01:52:20 +0100

    Worked on the adventure section

--- src/components/Adventure.jsx
+++ src/components/Adventure.jsx
@@ -1,41 +1,66 @@
-import React, {useState, useEffect}from "react";
-import pictures from '../components/pictures.js'
-
+import React, { useState, useEffect } from "react";
+import { pictures } from "../components/pictures.js";
+import { AiFillStar } from "react-icons/ai";
 
 function Adventure() {
-    const [data, setData] = useState([]);
-    const getData = () => {
-console.log(pictures.picture)
-        setData(pictures)
-
-    }
-    useEffect(() => {
-        getData()
-    }, [])
-
+  const [data, setData] = useState([]);
+  const getData = () => {
+    setData(pictures);
+  };
+  useEffect(() => {
+    getData();
+  }, []);
 
   return (
     <div>
+      <h1 className="font-bold text-center text-4xl mb-5">Inspiration for your next adventure</h1>
+      <div className="flex sm:flex-col md:flex-row flex-wrap justify-center">
+        {/* {
+data && data.length>0 && data.map((item)=><img src = {item.picture} alt = 'kkk'/>)
+} */}
+        {data.map((item) => {
+          return (
+            <div
+              key={item.id}
+              className=""
+              data-aos-anchor-easing="ease-in-out"
+            >
+              <div className="">
+                <div
+                  className=" p-3 border border-black/25 m-2 h-[350px] rounded-2xl"
+                  data-aos="fade-up"
+                  data-aos-duration="1400"
+                >
+                  <img
+                    className=" h-[250px] mb-3"
+                    data-aos="fade-up"
+                    data-aos-duration="1500"
+                    src={item.picture}
+                    alt="place"
+                  />
+                  <div className=" text-xs">
+                    <p className="float-right font-bold mb-2">1MBT per night</p>
 
+                    <p className="mb-2">Desert king</p>
+                    <p className="float-right">available for 2weeks stay</p>
 
-            {data.map((item) => {
-                console.log(item.picture)
-                return (
-
-
-                    <div key={item.id} className="card-group " data-aos-anchor-easing='ease-in-out'>
-
-                        <div className="card m-3 mt-3 mb-5" data-aos="fade-up" data-aos-duration='1400'>
-                            <img data-aos="fade-up" data-aos-duration='1500' src={item.picture} className="card-img-top" alt="grant type" />
-                            <div className="card-body">
-    
-
-                            </div>
-                        </div>
+                    <p className="mb-2">2345km away</p>
+                    <div className=" flex flex-row">
+                     
+                      <AiFillStar className="mr-2"/>
+                      <AiFillStar className="mr-2"/>
+                      <AiFillStar className="mr-2"/>
+                      <AiFillStar className="mr-2"/>
+                      <AiFillStar className="mr-2"/>
+                      
                     </div>
-
-                )
-            })}
+                  </div>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
--- src/components/pictures.js
+++ src/components/pictures.js
@@ -1,15 +1,35 @@
-let arrobj = [
+export const pictures = [
     {
         "id": 1,
          "picture": require("../Images/Frame1.png"),
    },
     {
         "id": 2,
-         "picture": require("../Images/Frame1.png"),
+         "picture": require("../Images/Frame2.png"),
    },
     {
         "id": 3,
+         "picture": require("../Images/Frame3.png"),
+   },
+    {
+        "id": 4,
+         "picture": require("../Images/Frame4.png"),
+   },
+    {
+        "id": 5,
          "picture": require("../Images/Frame1.png"),
    },
+    {
+        "id": 6,
+         "picture": require("../Images/Frame2.png"),
+   },
+    {
+        "id": 7,
+         "picture": require("../Images/Frame3.png"),
+   },
+    {
+        "id": 8,
+         "picture": require("../Images/Frame4.png"),
+   }
 
 ]
