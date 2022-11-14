3a0b79379943fc05aa87a5e8666eb15b5e382148
Tolulope Babatunde <babsbright69@gmail.com>
2022-11-14 16:01:44 +0100

    Worked on the place Component

---
+++ src/components/place.js
@@ -0,0 +1,67 @@
+export const pictures = [
+    {
+        "id": 1,
+         "picture": require("../Images/Frame1.png"),
+   },
+    {
+        "id": 2,
+         "picture": require("../Images/Frame2.png"),
+   },
+    {
+        "id": 3,
+         "picture": require("../Images/Frame3.png"),
+   },
+    {
+        "id": 4,
+         "picture": require("../Images/Frame4.png"),
+   },
+    {
+        "id": 5,
+         "picture": require("../Images/Frame5.png"),
+   },
+    {
+        "id": 6,
+         "picture": require("../Images/Frame6.png"),
+   },
+    {
+        "id": 7,
+         "picture": require("../Images/Frame7.png"),
+   },
+    {
+        "id": 8,
+         "picture": require("../Images/Frame8.png"),
+   },
+    {
+        "id": 9,
+         "picture": require("../Images/Frame1.png"),
+   },
+    {
+        "id": 10,
+         "picture": require("../Images/Frame2.png"),
+   },
+    {
+        "id": 11,
+         "picture": require("../Images/Frame3.png"),
+   },
+    {
+        "id": 12,
+         "picture": require("../Images/Frame4.png"),
+   },
+    {
+        "id": 13,
+         "picture": require("../Images/Frame5.png"),
+   },
+    {
+        "id": 14,
+         "picture": require("../Images/Frame6.png"),
+   },
+    {
+        "id": 15,
+         "picture": require("../Images/Frame7.png"),
+   },
+    {
+        "id": 16,
+         "picture": require("../Images/Frame8.png"),
+   }
+
+]
--- src/pages/Place.jsx
+++ src/pages/Place.jsx
@@ -1,9 +1,82 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { pictures } from "../components/place.js";
+import { AiFillStar } from "react-icons/ai";
+import Nav from "../components/Nav";
+import Footer from "../components/Footer";
 
 function Place() {
-    return (
-        <div>place</div>
-    )
+  const [data, setData] = useState([]);
+  const getData = () => {
+    setData(pictures);
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+  return (
+    <div>
+      <Nav />
+      <div className="mt-14 mb-24">
+        <ul className="flex md:flex-row justify-evenly items-center mx-16 mb-9">
+            <li className="text-sm">Restaurant</li>
+            <li className="text-sm">Cottage</li>
+            <li className="text-sm">Castle</li>
+            <li className="text-sm">Fantasy city</li>
+            <li className="text-sm">Beach</li>
+            <li className="text-sm">Cabins</li>
+            <li className="text-sm">Off-grid</li>
+            <li className="text-sm">Farm</li>
+            <button className="text-xs border border-black/25 rounded py-1.5 px-6">Location</button>
+        </ul>
+      
+        <div className="flex sm:flex-col md:flex-row flex-wrap justify-center">
+  
+          {data.map((item) => {
+            return (
+              <div
+                key={item.id}
+                className=""
+                data-aos-anchor-easing="ease-in-out"
+              >
+                <div className="">
+                  <div
+                    className=" p-3 border border-black/25 m-2 h-[350px] rounded-2xl"
+                    data-aos="fade-up"
+                    data-aos-duration="1400"
+                  >
+                    <img
+                      className=" h-[250px] mb-3"
+                      data-aos="fade-up"
+                      data-aos-duration="1500"
+                      src={item.picture}
+                      alt="place"
+                    />
+                    <div className=" text-xs">
+                      <p className="float-right font-bold mb-2">
+                        1MBT per night
+                      </p>
+
+                      <p className="mb-2">Desert king</p>
+                      <p className="float-right">available for 2weeks stay</p>
+
+                      <p className="mb-2">2345km away</p>
+                      <div className=" flex flex-row">
+                        <AiFillStar className="mr-2" />
+                        <AiFillStar className="mr-2" />
+                        <AiFillStar className="mr-2" />
+                        <AiFillStar className="mr-2" />
+                        <AiFillStar className="mr-2" />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
 }
 
-export default Place
+export default Place;
